327bb8563cc637ec06aca186fe77fe3605156721
BenjoBurger <90093905+BenjoBurger@users.noreply.github.com>
2024-03-29 23:06:36 +0800

    Resolve NewsOnTheGoTest.java Gradle issues

--- src/test/java/newsonthego/NewsOnTheGoTest.java
+++ src/test/java/newsonthego/NewsOnTheGoTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import newsonthego.commands.DailyNewsCommand;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
