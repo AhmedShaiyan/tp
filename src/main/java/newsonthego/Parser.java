1c879c61a76c9c807b93aa212d02467c0777e3bb
anneleong <anneleongsongen@gmail.com>
2024-04-15 16:00:36 +0800

    Enable extract command for filter function

--- src/main/java/newsonthego/Parser.java
+++ src/main/java/newsonthego/Parser.java
@@ -91,7 +91,6 @@ public class Parser {
             } else {
                 URLCommand.printArticleURL(line, list);
             }
-
             break;
         case SOURCE:
             if (topic >= 0) { //find source of news using index based on the current topic list shown to user
@@ -110,7 +109,6 @@ public class Parser {
                         "back command is invalid :(");
             }
             break;
-
         case QUOTE:
             String quote = quoteGenerator.getRandomQuote();
             printLine();
@@ -119,7 +117,11 @@ public class Parser {
             printLine();
             break;
         case EXTRACT:
-            ShowExtractCommand.showExtract(line, list);
+            if (topic >= 0) { //save using index based on the current topic list shown to user
+                ShowExtractCommand.showExtract(line, topics.get(topic).getRelatedNewsArticles());
+            } else {
+                ShowExtractCommand.showExtract(line, list);
+            }
             break;
         case BYE:
             UI.printBye();
